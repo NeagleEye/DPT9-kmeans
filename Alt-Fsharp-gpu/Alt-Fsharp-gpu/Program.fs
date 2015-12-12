af6c27949b51aa28a4a34e2d9b9b9d704203a6bd
NeagleEye <nkm182@gmail.com>
2015-12-12 18:36:23 +0100

    Fixed the OpenCL code..
    Fixed the OpenCL code.. removed an obsolute folder. and updated the test
    folder with a readme file.

---
+++ Alt-Fsharp-gpu/Alt-Fsharp-gpu/Program.fs
@@ -0,0 +1,25 @@
+﻿(*
+    This Program has been written with the idea that CPU and GPU together gives the best result.
+    Therefore the Distance calculation is done on the GPU and the Cluster Assign is done on the GPU,
+    the rest is done on the CPU and all the initializing.
+*)
+open System.IO
+let addLine (line:string) =     
+    use wr = StreamWriter("Kmeans-FOpenCL.txt", true)
+    wr.WriteLine(line)
+
+[<EntryPoint>]
+let main argv =
+
+    let mat = Kmeans.InitParameters.matrix
+    printfn "%A" mat
+
+    let difference = Kmeans.KmeansAlg.difference
+    printfn "%A" difference
+
+    let stopWatch = Kmeans.KmeansAlg.stopWatch
+
+    stopWatch.Stop()
+    printfn "%f" stopWatch.Elapsed.TotalMilliseconds
+    addLine(string stopWatch.Elapsed.TotalMilliseconds)
+    0 // return an integer exit code
---
+++ Alt-Fsharp-gpu/packages/Brahma.FSharp.0.3.5.0/Readme.txt
@@ -0,0 +1,10 @@
+Brahma.FSharp  (c) 2012, 2013 Semyon Grigorev <rsdpisuy@gmail.com>
+-------------------------------------------------------
+
+Brahma.FSharp is Brahma-based library for FSharp quotations to OpenCL translation.
+
+more information: https://sites.google.com/site/semathsrprojects/home/brahma-fsharp
+
+NuGet package: https://nuget.org/packages/Brahma.FSharp/
+
+G+ community: https://plus.google.com/u/0/communities/111965488041706059072
